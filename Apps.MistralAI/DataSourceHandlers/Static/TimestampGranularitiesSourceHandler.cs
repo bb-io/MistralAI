13102379088418f6455a651462012783853ee204
Yelyzaveta Kuzma <yelyzaveta.kuzma@gmail.com>
2026-02-25 12:10:03 +0200

    Added audio action 'Create transcription'

---
+++ Apps.MistralAI/DataSourceHandlers/Static/TimestampGranularitiesSourceHandler.cs
@@ -0,0 +1,16 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.MistralAI.DataSourceHandlers.Static;
+
+public class TimestampGranularitiesSourceHandler : IStaticDataSourceItemHandler
+{
+    public IEnumerable<DataSourceItem> GetData()
+    {
+        return new List<DataSourceItem>
+        {
+            new( "word", "Word" ),
+            new( "segment", "Segment"),
+        };
+    }
+}
---
+++ Apps.MistralAI/Models/Requests/TranscriptionRequest.cs
@@ -0,0 +1,31 @@
+﻿using Apps.MistralAI.DataSourceHandlers;
+using Apps.MistralAI.DataSourceHandlers.Static;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.MistralAI.Models.Requests
+{
+    public class TranscriptionRequest
+    {
+        [Display("File")]
+        public required FileReference File { get; set; }
+        
+        [Display("Model")]
+        [DataSource(typeof(ModelsDataHandler))]
+        public required string Model { get; set; }      
+        
+        [Display("Source language", Description = "Optional. If not set, Mistral will infer it from the content.")]
+        [DataSource(typeof(LocaleDataSourceHandler))]
+        public string? SourceLanguage { get; set; }
+
+        [Display("Temperature")]
+        [StaticDataSource(typeof(TemperatureDataSourceHandler))]
+        public float? Temperature { get; set; }
+        
+        [Display("Timestamp granularities")]
+        [StaticDataSource(typeof(TimestampGranularitiesSourceHandler))]
+        public string? TimestampGranularities { get; set; }
+    }
+}
